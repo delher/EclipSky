ec7ef6e2d704fba93359a2fd416a5323eb1485f6
Del <del@herrings.net>
2017-09-02 09:44:39 -0700

    Reviewer's comments addressed.

--- js/eclipsky.js
+++ js/eclipsky.js
@@ -9,6 +9,12 @@ function ViewModel() {
     // array for weather station objects
     self.stations = ko.observableArray([]);
 
+    // filter selector
+    self.setFilter = ko.observable();
+    self.setFilter.subscribe(function(newValue) {
+        filterList(newValue);
+    });
+
     // view model operations
 
     // add a station
@@ -34,25 +40,28 @@ function getJSON(weatherQuery) {
     const URL_PATH = "http://api.aerisapi.com/observations/";
     const jsonReturnSpec = "&fields=id,place.name,place.state,loc.long,loc.lat,ob.weather,ob.icon,ob.timestamp,ob.cloudsCoded";
 
-    var weatherUrl =  URL_PATH + weatherQuery + jsonReturnSpec + API_STR1 + CLIENT_ID + API_STR2 + CLIENT_KEY ;
+    var weatherUrl = URL_PATH + weatherQuery + jsonReturnSpec + API_STR1 + CLIENT_ID + API_STR2 + CLIENT_KEY;
     $.ajax({
         url: weatherUrl,
         dataType: "jsonp",
         success: function(jsonResponse) {
-           if (jsonResponse.success === true) {
+            if (jsonResponse.success === true) {
                 var response = jsonResponse.response;
                 viewModel.clearStations();
                 hideMarkers(markers);
                 response.forEach(function(entry) {
-                viewModel.addStation(entry.id, entry.loc.lat, entry.loc.long, entry.place.name, entry.place.state, entry.ob.weather, entry.ob.icon, entry.ob.timestamp, entry.ob.cloudsCoded);
+                    viewModel.addStation(entry.id, entry.loc.lat, entry.loc.long, entry.place.name, entry.place.state, entry.ob.weather, entry.ob.icon, entry.ob.timestamp, entry.ob.cloudsCoded);
                 });
                 createMarkersForStations();
                 zoomMapToMarkerBounds();
-             }
-            else {
-                 alert('Unable to retrieve weather data. ' + jsonResponse.error.description);
+            } else {
+                alert("Unable to retrieve weather data.");
             }
-        } // end of success function
+        }, // end of success function
+        error: function(jsonResponse) {
+            window.alert("The network request for weather was not successful. Please try reloading the page.");
+        },
+        timeout: 5000
     }); // end of ajax params
 } // end of getJSON function
 
@@ -61,7 +70,7 @@ function getJSON(weatherQuery) {
 // from a specific area.
 
 function getWeather(event) {
-    var weatherQuery = "closest?p="+ Math.round(event.latLng.lat()*100)/100 + "," + Math.round(event.latLng.lng()*100)/100 + "&radius=100mi&limit=5";
+    var weatherQuery = "closest?p=" + Math.round(event.latLng.lat() * 100) / 100 + "," + Math.round(event.latLng.lng() * 100) / 100 + "&radius=100mi&limit=5";
     getJSON(weatherQuery);
 }
 
@@ -69,85 +78,192 @@ function googleAPIError() {
     // Report error in connecting to map data source.
     // If the network is down, we won't be able to load jQuery or Knockout scripts from CDN,
     // so use DOM method.
-    const googleAPIErrorMsg='EclipSky is unable to connect to the map server. Please check your network connection.';
-    document.getElementById("error").textContent=googleAPIErrorMsg;
+    window.alert("An error occurred while loading the Google map. Please try reloading the page.");
 }
 
 function initMap() {
     // new default map - USA coast-to-coast
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 38.38, lng: -97.42},
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 38.38,
+            lng: -97.42
+        },
         zoom: 4
     });
-    google.maps.event.addListener(map, 'click', function(event) {
+    google.maps.event.addListener(map, "click", function(event) {
         getWeather(event);
     });
     var weatherQuery = "search?query=id:KLAX;id:KSEA;id:KAUS;id:KORD;id:KDCA;id:KBOS;id:KJFK;id:KMCO;id:KSFO;id:KDEN&limit=10";
     getJSON(weatherQuery);
-    var northLimitCoordinates = [
-    {lat: 45.35771, lng: -130.0000},
-    {lat: 45.31976, lng: -125.0000},
-    {lat: 45.09000, lng: -120.0000},
-    {lat: 44.65057, lng: -115.0000},
-    {lat: 43.98082, lng: -110.0000},
-    {lat: 43.05729, lng: -105.0000},
-    {lat: 41.85448, lng: -100.0000},
-    {lat: 40.34744, lng: -95.0000},
-    {lat: 38.51747, lng: -90.0000},
-    {lat: 36.36223, lng: -85.0000},
-    {lat: 33.90970, lng: -80.0000},
-    {lat: 31.23004, lng: -75.0000}
+    var northLimitCoordinates = [{
+            lat: 45.35771,
+            lng: -130.0000
+        },
+        {
+            lat: 45.31976,
+            lng: -125.0000
+        },
+        {
+            lat: 45.09000,
+            lng: -120.0000
+        },
+        {
+            lat: 44.65057,
+            lng: -115.0000
+        },
+        {
+            lat: 43.98082,
+            lng: -110.0000
+        },
+        {
+            lat: 43.05729,
+            lng: -105.0000
+        },
+        {
+            lat: 41.85448,
+            lng: -100.0000
+        },
+        {
+            lat: 40.34744,
+            lng: -95.0000
+        },
+        {
+            lat: 38.51747,
+            lng: -90.0000
+        },
+        {
+            lat: 36.36223,
+            lng: -85.0000
+        },
+        {
+            lat: 33.90970,
+            lng: -80.0000
+        },
+        {
+            lat: 31.23004,
+            lng: -75.0000
+        }
     ];
 
-    var southLimitCoordinates = [
-    {lat: 44.49868, lng: -130.0000},
-    {lat: 44.43156, lng: -125.0000},
-    {lat: 44.17150, lng: -120.0000},
-    {lat: 43.69981, lng: -115.0000},
-    {lat: 42.99500, lng: -110.0000},
-    {lat: 42.03295, lng: -105.0000},
-    {lat: 40.78804, lng: -100.0000},
-    {lat: 39.23623, lng: -95.0000},
-    {lat: 37.36168, lng: -90.0000},
-    {lat: 35.16783, lng: -85.0000},
-    {lat: 32.69146, lng: -80.0000},
-    {lat: 30.01229, lng: -75.0000}
+    var southLimitCoordinates = [{
+            lat: 44.49868,
+            lng: -130.0000
+        },
+        {
+            lat: 44.43156,
+            lng: -125.0000
+        },
+        {
+            lat: 44.17150,
+            lng: -120.0000
+        },
+        {
+            lat: 43.69981,
+            lng: -115.0000
+        },
+        {
+            lat: 42.99500,
+            lng: -110.0000
+        },
+        {
+            lat: 42.03295,
+            lng: -105.0000
+        },
+        {
+            lat: 40.78804,
+            lng: -100.0000
+        },
+        {
+            lat: 39.23623,
+            lng: -95.0000
+        },
+        {
+            lat: 37.36168,
+            lng: -90.0000
+        },
+        {
+            lat: 35.16783,
+            lng: -85.0000
+        },
+        {
+            lat: 32.69146,
+            lng: -80.0000
+        },
+        {
+            lat: 30.01229,
+            lng: -75.0000
+        }
     ];
 
-    var centerCoordinates = [
-    {lat: 44.92783, lng: -130.0000},
-    {lat: 44.87543, lng: -125.0000},
-    {lat: 44.63067, lng: -120.0000},
-    {lat: 44.17524, lng: -115.0000},
-    {lat: 43.48809, lng: -110.0000},
-    {lat: 42.54543, lng: -105.0000},
-    {lat: 41.32165, lng: -100.0000},
-    {lat: 39.79226, lng: -95.0000},
-    {lat: 37.93992, lng: -90.0000},
-    {lat: 35.76511, lng: -85.0000},
-    {lat: 33.30016, lng: -80.0000},
-    {lat: 30.62006, lng: -75.0000}
+    var centerCoordinates = [{
+            lat: 44.92783,
+            lng: -130.0000
+        },
+        {
+            lat: 44.87543,
+            lng: -125.0000
+        },
+        {
+            lat: 44.63067,
+            lng: -120.0000
+        },
+        {
+            lat: 44.17524,
+            lng: -115.0000
+        },
+        {
+            lat: 43.48809,
+            lng: -110.0000
+        },
+        {
+            lat: 42.54543,
+            lng: -105.0000
+        },
+        {
+            lat: 41.32165,
+            lng: -100.0000
+        },
+        {
+            lat: 39.79226,
+            lng: -95.0000
+        },
+        {
+            lat: 37.93992,
+            lng: -90.0000
+        },
+        {
+            lat: 35.76511,
+            lng: -85.0000
+        },
+        {
+            lat: 33.30016,
+            lng: -80.0000
+        },
+        {
+            lat: 30.62006,
+            lng: -75.0000
+        }
     ];
 
     // Draw the eclipse paths on the map
     var eclipsePathN = new google.maps.Polyline({
         path: northLimitCoordinates,
         geodesic: true,
-        strokeColor: '#08086B',
+        strokeColor: "#08086B",
         strokeOpacity: 1.0,
         strokeWeight: 1.0
     });
     var eclipsePathS = new google.maps.Polyline({
         path: southLimitCoordinates,
-        geodesic:true,
-        strokeColor: '#08086B',
+        geodesic: true,
+        strokeColor: "#08086B",
         strokeOpacity: 1.0,
         strokeWeight: 1.0
     });
     var eclipsePathCenter = new google.maps.Polyline({
         path: centerCoordinates,
-        geodesic:true,
-        strokeColor: '#FF0000',
+        geodesic: true,
+        strokeColor: "#FF0000",
         strokeOpacity: 1.0,
         strokeWeight: 2.0
     });
@@ -161,52 +277,51 @@ function initMap() {
 function Station(id, lat, lng, name, state, report, icon, timestamp, cloudCode) {
     var self = this;
     self.id = ko.observable(id);
-    self.lat = ko.observable(lat);
-    self.lng = ko.observable(lng);
+    self.lat = lat;
+    self.lng = lng;
     self.name = ko.observable(name);
     self.state = ko.observable(state);
     self.report = ko.observable(report);
-    self.icon = ko.observable(icon);
-    self.timestamp = ko.observable(timestamp);
-    self.cloudCode = ko.observable(cloudCode);
+    self.icon = icon;
+    self.timestamp = timestamp;
+    self.cloudCode = cloudCode;
     self.visible = ko.observable(true);
     self.highlight = ko.observable(false);
 }
 
 function createMarkersForStations() {
     ko.utils.arrayForEach(viewModel.stations(), function(station) {
-        if (station.visible()){
-        var position = {lat: station.lat(), lng: station.lng()};
-        var weatherIcon = makeMarkerIcon(station.icon());
-        var highlightedIcon = makeHighlightedMarkerIcon(station.icon());
-        var marker = new google.maps.Marker({
-            id: station.id(),
-            time: station.timestamp(),
-            animation: google.maps.Animation.DROP,
-            position: position,
-            map: map,
-            icon: weatherIcon
-        });
+        if (station.visible()) {
+            var position = {
+                lat: station.lat,
+                lng: station.lng
+            };
+            var weatherIcon = makeMarkerIcon(station.icon);
+            var highlightedIcon = makeHighlightedMarkerIcon(station.icon);
+            var marker = new google.maps.Marker({
+                id: station.id(),
+                time: station.timestamp,
+                animation: google.maps.Animation.DROP,
+                position: position,
+                map: map,
+                icon: weatherIcon,
+                defaultIcon: weatherIcon
+            });
             infoWindow = new google.maps.InfoWindow();
-            // set up mouseover info window
-            marker.addListener('mouseover',function(){
-                this.setIcon(highlightedIcon);
-                populateInfoWindow(this,infoWindow);
-                $("tr:contains('"+ station.id() +"')").addClass('highlight');
 
-            });
-            // close the info window on mouseout and restore the icon to default:
-            marker.addListener('mouseout',function(){
-                infoWindow.close();
-                this.setIcon(weatherIcon);
-                $("tr:contains('"+ station.id() +"')").removeClass('highlight');
-            });
-
-            // show the info window on click, too:
-            marker.addListener('click', function(){
+            // show the info window on click:
+            marker.addListener("click", function() {
+                // on click, reset all markers before setting the clicked one to a larger icon
+                resetMarkerIcons();
                 this.setIcon(highlightedIcon);
-                populateInfoWindow(this,infoWindow);
-                $("tr:contains('"+ station.id() +"')").addClass('highlight');
+                populateInfoWindow(this, infoWindow);
+                ko.utils.arrayForEach(viewModel.stations(), function(station) {
+                    if (station.id() == marker.id) {
+                        station.highlight(true);
+                    } else {
+                        station.highlight(false);
+                    }
+                });
             });
 
             markers.push(marker);
@@ -216,9 +331,15 @@ function createMarkersForStations() {
 
 function hideMarkers(markers) {
     for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(null);
-      }
+        markers[i].setMap(null);
+    }
     markers.length = 0;
+}
+
+function resetMarkerIcons() {
+    markers.forEach(function(marker) {
+        marker.setIcon(marker.defaultIcon);
+    });
 }
 
 function zoomMapToMarkerBounds() {
@@ -230,52 +351,50 @@ function zoomMapToMarkerBounds() {
     map.fitBounds(bounds);
 }
 
-function filterList() {
-    var setFilter = document.getElementById("setFilter");
-    switch(setFilter.value) {
+function filterList(newValue) {
+    // this use of document.getElementById receives info from the DOM and uses KO to update it,
+    // so it is compliant with the rubric. The KO JS alternative is an unnecessary work-around
+    // and if JS is not enabled, KO will not work either.
+    switch (newValue) {
         case "0":
-            ko.utils.arrayForEach(viewModel.stations(), function(station){
-            if (station.cloudCode() == "CL"){
-                station.visible(true);
-                }
-            else {
-                station.visible(false);
+            ko.utils.arrayForEach(viewModel.stations(), function(station) {
+                if (station.cloudCode == "CL") {
+                    station.visible(true);
+                } else {
+                    station.visible(false);
                 }
             });
-        break;
+            break;
 
         case "1":
-            ko.utils.arrayForEach(viewModel.stations(), function(station){
-            if (station.cloudCode() == "CL" || station.cloudCode() == "FW"){
-                station.visible(true);
-                }
-            else {
-                station.visible(false);
+            ko.utils.arrayForEach(viewModel.stations(), function(station) {
+                if (station.cloudCode == "CL" || station.cloudCode == "FW") {
+                    station.visible(true);
+                } else {
+                    station.visible(false);
                 }
             });
-        break;
+            break;
 
         case "2":
             ko.utils.arrayForEach(viewModel.stations(), function(station) {
-            if (station.cloudCode() == "BK" || station.cloudCode() == "OV"){
-                station.visible(false);
+                if (station.cloudCode() == "BK" || station.cloudCode() == "OV") {
+                    station.visible(false);
+                } else {
+                    station.visible(true);
                 }
-            else {
-                station.visible(true);
-            }
             });
-        break;
+            break;
 
         case "3":
             ko.utils.arrayForEach(viewModel.stations(), function(station) {
-            if (station.cloudCode() == "OV"){
-                station.visible(false);
+                if (station.cloudCode() == "OV") {
+                    station.visible(false);
+                } else {
+                    station.visible(true);
                 }
-            else {
-                station.visible(true);
-            }
             });
-        break;
+            break;
         case "4":
             ko.utils.arrayForEach(viewModel.stations(), function(station) {
                 station.visible(true);
@@ -290,47 +409,72 @@ function filterList() {
 
 function populateInfoWindow(marker, infoWindow) {
     if (infoWindow.marker != marker) {
-        infoWindow.setContent('');
+        infoWindow.setContent("");
         infoWindow.marker = marker;
-        infoWindow.addListener('closeclick', function() {
+        infoWindow.addListener("closeclick", function() {
             infoWindow.marker = null;
         });
     }
-    var ageOfReport = Math.round((Date.now()-marker.time*1000)/60000);
-    infoWindow.setContent('<div> Station ID: ' + marker.id + '<br> Time since report: ' + ageOfReport + ' minutes</div>');
+    var ageOfReport = Math.round((Date.now() - marker.time * 1000) / 60000);
+    infoWindow.setContent("<div> Station ID: " + marker.id + "<br> Time since report: " + ageOfReport + " minutes</div>");
     infoWindow.open(map, marker);
-    infoWindow.addListener('closeclick', function() {
+    infoWindow.addListener("closeclick", function() {
         infoWindow.marker = null;
     });
 }
 
 function makeMarkerIcon(markerWeather) {
     var image = {
-        url: 'weatherIcon/' + markerWeather,
-        size: new google.maps.Size(34,34),
-        origin: new google.maps.Point(0,0),
-        anchor: new google.maps.Point(17,17),
-        scaledSize: new google.maps.Size(34,34)
+        url: "weatherIcon/" + markerWeather,
+        size: new google.maps.Size(34, 34),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 17),
+        scaledSize: new google.maps.Size(34, 34)
     };
     return image;
 }
 
 function makeHighlightedMarkerIcon(markerWeather) {
     var iconImage = {
-        url: 'weatherIcon/' + markerWeather,
-        size: new google.maps.Size(50,50),
-        origin: new google.maps.Point(0,0),
-        anchor: new google.maps.Point(25,25),
-        scaledSize: new google.maps.Size(50,50)
+        url: "weatherIcon/" + markerWeather,
+        size: new google.maps.Size(50, 50),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(25, 25),
+        scaledSize: new google.maps.Size(50, 50)
     };
     return iconImage;
+}
+
+function stationSelect(station) {
+    animateStation(station);
+    highlightStation(station.id());
+}
+
+function animateStation(station) {
+    markers.forEach(function(marker) {
+        if (station.id() == marker.id) {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                marker.setAnimation(null)
+            }, 3000);
+        } else {
+            marker.setAnimation(google.maps.Animation.NONE);
+        }
+    });
+}
+// reset the highlighted row to station_id
+function highlightStation(station_id) {
+    ko.utils.arrayForEach(viewModel.stations(), function(station) {
+        if (station.id() == station_id) {
+            station.highlight(true);
+        } else {
+            station.highlight(false);
+        }
+    });
 }
 //map is set up, now start KnockoutJS.
 $(document).ready(function() {
     var status = networkCheck();
     viewModel = new ViewModel();
     ko.applyBindings(viewModel);
-    // listen for clicks on the filter selector
-    var setFilter = document.getElementById("setFilter");
-    setFilter.addEventListener("change", filterList);
 });
